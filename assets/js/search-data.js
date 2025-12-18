ede27f13a10399e45b61c823e3923ad969a16770
ams939 <38046122+ams939@users.noreply.github.com>
2025-12-18 15:01:51 +0000

    Deploying to gh-pages from @ ams939/ams939.github.io@eebafa62eb5a29ff5e2ea3d5c3f7daa34337a4d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-the-aleatoric-blog",
+          title: "The Aleatoric Blog",
           description: "",
           section: "Navigation",
           handler: () => {
